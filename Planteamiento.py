caffd97c3f8a68d58d610c65096b1fb290d57172
Diego Calderón <>
2021-05-02 00:54:19 +0000

    Proyecto

---
+++ Planteamiento.py
@@ -0,0 +1,49 @@
+"""
+Entradas
+Usuario-->input-->U
+Contraseña-->int-->C
+Opcion 1-->int-->1
+Opcion 2-->int-->2
+Opcion 3-->int-->3
+Salidas
+Operacion realizada-->float-->OR
+"""
+lista=["CAMILO","2345","JOHAN","1136"]
+while True:
+  u=input("digite usuario: ")
+	
+  c=input("digite contraseña: ") 
+  if (u==lista[0]):
+    if(c==lista[1]):
+      print("Bienvenido a cajero EAN: ")
+    else
+     print("no se reconoce contrasña: ")
+  else
+    print("no se reconoce usuario: ")
+      break 
+
+  Retirar dinero=1
+  Consultar saldo=2
+  Transferir dinero=3
+  O=int(input("Digite según la operación que desea realizar: Retirar dinero (1), consultar saldo (2), transferir dinero a otra cuenta (3)"))
+
+
+  if(O==1):
+    Cantidad a retirar=float(input("Digite la cantidad que desea retirar."))
+    print("Operacion realizada")
+   
+  elif(O==2):
+   consultar=int(input("Si requiere consultar el saldo en su cuenta digite (6)"))
+    if(verificar==6):
+      #Según el monto de cada dependencia 
+      monto=10000000
+      saldo=monto-cantidad a retirar
+      print("Saldo actual: "+str(saldo))
+  elif(0==3):
+    Cuenta2=float(input("Ingrese el numero de cuenta a quien desea tranferir: "))
+    
+    Cuenta2=x
+
+    if(cuenta2==x):
+      print("Digite el dinero a transferir: ")
+      555555
