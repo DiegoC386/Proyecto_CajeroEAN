e1295d2201b760638e551ba5c64d45e92cdef134
DavidAlbaHernandez <>
2021-05-02 00:53:50 +0000

    avance1.1

--- Desarrollo.py
+++ Desarrollo.py
@@ -1,58 +1,73 @@
 """
-Entradas
+#Entradas
 Usuario-->input-->U
-Contraseña-->int-->C
+Contraseña-->input-->C
 Opcion 1-->int-->1
 Opcion 2-->int-->2
 Opcion 3-->int-->3
-Salidas
-Operacion realizada-->float-->OR
+Opcion 3-->int-->4
+#Salidas
+Operaciones-->float-->O.realizadas
 """
-lista=["CAMILO","2345","JOHAN","1136"]
-while True:
-  u=input("digite usuario: ")
-	
-  c=input("digite contraseña: ") 
+print("......................................:CAJERO.EAN:..................................")
+print("Por la seguridad del usuario solo es permitido 3 intentos para ingresar a su cuenta")
+#CAJERO EAN
+print("")
+contador=1
+lista=["BancoEAN","1234"]
+while contador <=3:
+  u=input("Digite su usuario: ")
+  c=input("Digite contraseña: ") 
   if (u==lista[0]):
     if(c==lista[1]):
+      Saldoban=100000
       print("Bienvenido a cajero EAN: ")
-    else
-     print("no se reconoce contrasña: ")
-  else
-    print("no se reconoce usuario: ")
-      break 
-
-  Retirar dinero=1
-  Consultar saldo=2
-  Transferir dinero=3
-  O=int(input("Digite según la operación que desea realizar: Retirar dinero (1), consultar saldo (2), transferir dinero a otra cuenta (3)"))
-
-
-  if(O==1):
-    Cantidad a retirar=float(input("Digite la cantidad que desea retirar."))
-    print("Operacion realizada")
-   
-  elif(O==2):
-   consultar=int(input("Si requiere consultar el saldo en su cuenta digite (6)"))
-    if(verificar==6):
-      #Según el monto de cada dependencia 
-      monto=10000000
-      saldo=monto-cantidad a retirar
-      print("Saldo actual: "+str(saldo))
-  elif(0==3):
-    Cuenta2=float(input("Ingrese el numero de cuenta a quien desea tranferir: "))
-    
-    Cuenta2=x
-
-    if(cuenta2==x):
-      print("Digite el dinero a transferir: ")
-      555555
-      
-      
-
-    
-    
-  
+      contador==4
+      print(".....................................EAN.................................")
+    else:
+      print("contraseña incorrecta ")
+  else:
+       print("usuario no recococido")
+       if contador==3:
+           print("comuniquese con el administrrador")
+  contador=contador +1
+  break
+print("\t.:MENU:.")
+print("")
+print("1. consultar saldo")
+print("2. Retirar dinero")
+print("3. transferir dinero a otra cuenta")
+print("4. SALIR")
+print(".....................................EAN.................................")
+Opcion=int(input("Digite una opcion de MENU: "))
+print("")
+if(Opcion==2):
+    Retiro=float(input("Digite la cantidad que desea retirar: "))
+    print(f"Operacion realizada, su saldo actual es: {Saldoban-Retiro}")
+    if Retiro>Saldoban:
+      print("noTIENE el saldo digitado")
+    else:
+      print(f"Dinero en la cuenta es: {Saldoban-Retiro}")
+      print(".....................................EAN.................................") 
+elif(Opcion==1):
+  print(f"Dinero en la cuenta es: {Saldoban-Retiro}")
+  print(".....................................EAN.................................")
+  print("")
+elif(Opcion==3):
+ Cuenta2=float(input("Ingrese el numero de cuenta a quien desea tranferir: "))
+ if(Cuenta2==12345):
+      trans=float(input("Digite la cantidad que desea  tranferir: "))
+      print(f"Operacion realizada, su saldo actual es: {Saldoban-trans}")
+      if trans>Saldoban:
+        print("noTIENE el saldo digitado")
+        print(".....................................EAN.................................")
+        print("")
+elif(Opcion==4):
+  print("gracias por usar el cajero EAN")
+  print(".....................................EAN.................................")
+  print("")
+else:
+  print("ERROR, Opcion no valida para el cajero EAN")
 
     
   
