8f680296745e5d7daa85457c0455baecb3b35aa4
DavidAlbaHernandez <>
2021-05-02 22:14:16 +0000

    descrpcion

--- Avances/Planteamiento.py
+++ Avances/Planteamiento.py
@@ -1,77 +1,11 @@
 """
 #Entradas
-Usuario-->input-->U
-Contraseña-->input-->C
+Usuario-->input-->us
+Contraseña-->input-->co
 Opcion 1-->int-->1
 Opcion 2-->int-->2
 Opcion 3-->int-->3
 Opcion 3-->int-->4
 #Salidas
-Operaciones-->float-->O.realizadas
+Operaciones-->int-->O.realizadas
 """
-Cuentas=[]
-print("......................................:CAJERO.EAN:..................................")
-print("Por la seguridad del usuario solo es permitido 3 intentos para ingresar a su cuenta")
-#CAJERO EAN
-usuario="DIEGO"
-passw="1234"
-saldo=500000
-saldo2=100000
-cont=0
-conectado=bool;
-while cont<3:
-    us=input("Ingrese usuario: ");
-    co=input("Ingrese contraseña: ");
-    if us==usuario and passw==co:
-        print ("Bienvenido al sistema")
-        conectado=True
-        break
-    else:
-        cont=cont+1;
-        print ("Usuario y contraseña incorrecta")
-        conectado=False
-while conectado: 
-  print("\t.:MENU:.")
-  print("")
-  print("1. Consultar saldo")
-  print("2. Retirar dinero")
-  print("3. Transferir")
-  print("4. SALIR")
-  print(".....................................EAN.................................")
-  Opcion=int(input("Digite una opcion de MENU: "))
-  print("")
-  break
-if(Opcion==1):
-	print ("Su saldo es:", saldo)
-	print(".....................................EAN.................................")
-	print("")
-	print ("\t.:MENU:.")
-	print("")
-	print ("2. Retirar dinero")
-	print ("3. Transferir")
-	print ("4. SALIR")
-	Opcion=int(input("Digite una opcion de MENU: "))
-	print("")
-if(Opcion==2):
-    Retiro=int(input("Digite la cantidad que desea retirar: "))
-    print(f"Operacion realizada, su saldo actual es: {saldo-Retiro}")
-    if Retiro>saldo:
-      print("No tiene saldo disponible")
-      print(".....................................EAN.................................")
-      print("")
-elif(Opcion==3):
-      cu2=input("Ingrese cuenta a Depositar: ")
-      Cuentas.append(cu2)    
-      monto=int(input("Ingrese monto a Transferir: "))
-      saldo=saldo-monto
-      saldo2=saldo2+monto
-      print ("Se han transferido", monto,"pesos a la cuenta",cu2)
-      print ("El nuevo saldo es:",saldo2)
-      print(".....................................EAN.................................")
-      print("")
-elif(Opcion==4):
-    print("Gracias por usar el Cajero EAN")
-    print(".....................................EAN.................................")
-    print("")
-else:
-      print("ERROR, Opcion no valida para el cajero EAN")
